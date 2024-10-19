a4a95e7a9feffb9343704fd90acfbbd39eff4738
Jubilantrich <jubilantrich@gmail.com>
2024-10-19 17:52:11 +0000

    19th Oct 2024 java class update

--- src/TRAINING/table_connectivity.java
+++ src/TRAINING/table_connectivity.java
@@ -85,6 +85,7 @@ public class table_connectivity extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         biodata_table = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -113,6 +114,13 @@ public class table_connectivity extends javax.swing.JFrame {
             }
         });
 
+        jButton2.setText("EDIT");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -124,13 +132,17 @@ public class table_connectivity extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(18, 18, 18)
                 .addComponent(jButton1)
+                .addGap(140, 140, 140)
+                .addComponent(jButton2)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(9, 9, 9)
-                .addComponent(jButton1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 576, Short.MAX_VALUE)
                 .addContainerGap())
@@ -153,6 +165,25 @@ public class table_connectivity extends javax.swing.JFrame {
 
 
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+       
+        DefaultTableModel biomodel = (DefaultTableModel) biodata_table.getModel();
+        int i = biodata_table.getSelectedRow();
+        String id = biomodel.getValueAt(i,0).toString();        
+        String name = biomodel.getValueAt(i,1).toString();  
+        String age = biomodel.getValueAt(i,2).toString(); 
+        String gender = biomodel.getValueAt(i,3).toString(); 
+        
+        user_update up = new user_update(); // call in the object user update fom
+        up.setVisible(true);
+        pack();
+        
+        up.UID.setText(id);
+        up.NAME.setText(name);
+        up.AGE.setText(age);
+        up.GENDER.setText(gender);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -192,6 +223,7 @@ public class table_connectivity extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public javax.swing.JTable biodata_table;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 
